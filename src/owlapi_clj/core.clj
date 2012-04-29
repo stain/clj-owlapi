a0b4fa466d06d4720f72fa5c66dfa601c738cb89
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-04-29 22:34:39 +0100

    owlapi-clj

---
+++ src/owlapi_clj/core.clj
@@ -0,0 +1,1 @@
+(ns owlapi-clj.core)
---
+++ test/owlapi_clj/test/core.clj
@@ -0,0 +1,6 @@
+(ns owlapi-clj.test.core
+  (:use [owlapi-clj.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
