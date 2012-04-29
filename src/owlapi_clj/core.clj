c2286c5ababb6fced72e84642ed7dc02a0d8a474
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-04-29 23:43:53 +0100

    testing the OWL api

--- src/owlapi_clj/core.clj
+++ src/owlapi_clj/core.clj
@@ -1,1 +1,22 @@
-(ns owlapi-clj.core)
+(ns owlapi-clj.core
+  (:import 
+     (org.semanticweb.owlapi.apibinding OWLManager)
+     (org.semanticweb.owlapi.model IRI)
+  )
+)
+
+(defn owl-manager [] (OWLManager/createOWLOntologyManager))
+	
+(defn load-ontology 
+	[uri] (load-ontology uri (owl-manager))
+	[uri manager] [(.loadOntologyFromOntologyDocument manager (IRI/create uri)) manager])
+
+(defn remove-ontology! [[ontology manager]]
+	(.removeOntology manager ontology))
+
+(defn ontology-document-uri [[ontology manager]]
+	(.getOntologyDocumentIRI manager ))
+
+(defn save-ontology
+      [[ontology manager] file] (.saveOntology manager ontology (IRI/create (.toURI file))))
+
--- test/owlapi_clj/test/core.clj
+++ test/owlapi_clj/test/core.clj
@@ -1,6 +1,21 @@
 (ns owlapi-clj.test.core
   (:use [owlapi-clj.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:import (java.io File)))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(defn load-pizza 
+	[] (load-ontology "http://www.co-ode.org/ontologies/pizza/pizza.owl"))
+
+(deftest load-pizza-owl
+  (is true (load-pizza)))
+
+(deftest load-then-remove-pizza-owl
+  (is true (remove-ontology! (load-pizza))))
+
+(deftest save-pizza
+  (def file (doto (File/createTempFile "pizza" ".owl") (.delete) (.deleteOnExit)))
+  (is false (.exists file))
+  (save-ontology (load-pizza) file)
+  (is true (.exists file))
+)
+
