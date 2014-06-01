20cf8e7db0617d9e8a4902dcda9e0e8be09ea034
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-06-01 21:59:59 +0100

    Updated dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject owlapi-clj "1.0.0-SNAPSHOT"
+(defproject owlapi-clj "1.0.1-SNAPSHOT"
   :description "OWL API wrapper for Clojure"
   :dependencies [
-                 [org.clojure/clojure "1.3.0"]
-;                 [org.clojure/clojure-contrib "1.3.0"]
-                 [net.sourceforge.owlapi/owlapi "3.2.4"]
+                 [org.clojure/clojure "1.6.0"]
+;                 [org.clojure/clojure-contrib "1.6.0"]
+                 [net.sourceforge.owlapi/owlapi-contract "3.5.0"]
 
                 ]
   :repositories {"owlapi-clj" "http://stain.github.com/owlapi-clj/repository/"}
