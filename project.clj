94a9e9fb1e9d7be483c1a9050ef117b17b460d2b
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-06-03 00:58:41 +0100

    metadata

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
 (defproject owlapi-clj "1.0.1-SNAPSHOT"
   :description "OWL API wrapper for Clojure"
+  :url "https://github.com/stain/owlapi-clj"  
   :dependencies [
                  [org.clojure/clojure "1.6.0"]
 ;                 [org.clojure/clojure-contrib "1.6.0"]
                  [net.sourceforge.owlapi/owlapi-contract "3.5.0"]
 
                 ]
-  :repositories {"owlapi-clj" "http://stain.github.com/owlapi-clj/repository/"}
+;  :repositories {"owlapi-clj" "http://stain.github.com/owlapi-clj/repository/"}
 
 )
