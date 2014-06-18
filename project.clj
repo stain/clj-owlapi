c615c7cc735539e3c3c326264838c13bf9c60a08
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-06-18 17:02:10 +0100

    dependency for owlapi-clj
    From https://github.com/stain/owlapi-jsonld

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
                  [org.clojure/clojure "1.6.0"]
 ;                 [org.clojure/clojure-contrib "1.6.0"]
                  [net.sourceforge.owlapi/owlapi-contract "3.5.0"]
+                 [org.clojars.stain/owlapi-jsonld "0.1.0"]
 
                 ]
 
