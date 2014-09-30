4d675f5031aca988127db2b33b9dc936a01ab18c
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-09-30 15:40:12 +0100

    sljf4j dependency to avoid null-logger

--- project.clj
+++ project.clj
@@ -6,7 +6,13 @@
 ;                 [org.clojure/clojure-contrib "1.6.0"]
                  [net.sourceforge.owlapi/owlapi-contract "3.5.0"]
                  [org.clojars.stain/owlapi-jsonld "0.1.0"]
-                 
+
                 ]
+  :profiles {
+    :dev { :dependencies [
+        [org.slf4j/slf4j-jdk14 "1.7.7"]
+      ]
+    }
+  }
 
 )
